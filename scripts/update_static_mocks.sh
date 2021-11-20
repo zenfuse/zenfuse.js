ecf210cefe8bedccb3f113e29a58a57e30be8347
Vyacheslav Melnikov <vyacheslav@izzqz.me>
2021-11-20 19:18:48 +0000

    Update static mocks

--- scripts/update_static_mocks.sh
+++ scripts/update_static_mocks.sh
@@ -1,1 +1,4 @@
-curl https://api.binance.com/api/v3/exchangeInfo > ./tests/binance/mocks/exchangeInfo.json
+echo '\n\nBinance -------';
+curl https://api.binance.com/api/v3/exchangeInfo > ./tests/binance/mocks/exchangeInfo.json;
+echo '\n\nFTX -----------';
+curl https://ftx.com/api/markets > ./tests/ftx/mocks/markets.json;
