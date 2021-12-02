ce5676ef5ce95a5ee783a0affa911e32a0621825
Vyacheslav Melnikov <vyacheslav@izzqz.me>
2021-12-02 17:22:11 +0000

    Change static mocks downloading

---
+++ scripts/download_mocks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function fetch {
+wget $1 \
+    --quiet \
+    --show-progress \
+    --no-cache \
+    --no-cookies \
+    --output-document $2;
+}
+
+echo '⬇ Downloading static mocks...'
+echo
+echo 'Binance -------'
+echo
+fetch https://api.binance.com/api/v3/exchangeInfo ./tests/binance/mocks/static/exchangeInfo.json;
+fetch https://api.binance.com/api/v3/ticker/price ./tests/binance/mocks/static/prices.json;
+echo
+echo
+echo 'FTX -----------'
+echo
+fetch https://ftx.com/api/markets ./tests/ftx/mocks/static/markets.json;
--- scripts/update_static_mocks.sh
+++
@@ -1,7 +0,0 @@
-echo '\n\nBinance -------';
-curl https://api.binance.com/api/v3/exchangeInfo > ./tests/binance/mocks/static/exchangeInfo.json;
-curl https://api.binance.com/api/v3/ticker/price > ./tests/binance/mocks/static/prices.json;
-
-
-echo '\n\nFTX -----------';
-curl https://ftx.com/api/markets > ./tests/ftx/mocks/static/markets.json;
