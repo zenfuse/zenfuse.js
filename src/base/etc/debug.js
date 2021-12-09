70da5dbda39b1bb9bb17c94f82695f986c8540fd
Vyacheslav Melnikov <vyacheslav@izzqz.me>
2021-12-09 16:55:24 +0000

    Add debugger logging

---
+++ src/base/etc/debug.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const inspector = require('inspector');
+
+const shouldDebugZenfuse = process.env.ZENFUSE_DEBUG === 'true';
+
+/**
+ * @type {ProxyHandler}
+ */
+const consoleHandler = {
+    apply: (target, _that, args) => {
+        if (!shouldDebugZenfuse) return;
+
+        if (args.every((i) => typeof i === 'string')) {
+            // args.unshift('zenfuse');
+        }
+
+        target(...args);
+    },
+};
+
+module.exports = {
+    info: new Proxy(inspector.console.info, consoleHandler),
+    log: new Proxy(inspector.console.log, consoleHandler),
+    warn: new Proxy(inspector.console.warn, consoleHandler),
+    error: new Proxy(inspector.console.error, consoleHandler),
+};
