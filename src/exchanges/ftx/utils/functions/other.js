ff4a169491d26dbfea5a6957aa70516805f9284e
Vyacheslav Melnikov <vyacheslav@izzqz.me>
2022-01-09 16:40:27 +0000

    Add createHmacSignature for FTX

--- src/exchanges/ftx/utils/functions/other.js
+++ src/exchanges/ftx/utils/functions/other.js
@@ -1,1 +1,20 @@
-module.exports = {};
+const { createHmac } = require('crypto');
+
+/**
+ * Create HMAC sicnature specific for FTX
+ *
+ * @param {object} params
+ * @param {number} params.ts UNIX Timestapm
+ * @param {string} params.method HTTP Method
+ * @param {string} params.path URL request path
+ * @param {object} [params.body] Request body
+ * @param {string} key HMAC Key
+ * @returns {stting} Hex HMAC Signature
+ */
+const createHmacSignature = (params, key) => {
+    const signaturePayload = Object.values(params).join('');
+
+    return createHmac('sha256', key).update(signaturePayload).digest('hex');
+};
+
+module.exports = { createHmacSignature };
--- src/exchanges/ftx/utils/utils.spec.js
+++ src/exchanges/ftx/utils/utils.spec.js
@@ -1,5 +1,32 @@
 const utils = require('.');
 
-describe('function()', () => {
-    it('', () => {})
+describe('createHmacSignature()', () => {
+    const { createHmacSignature } = utils;
+
+    it('should return valid signature', () => {
+        const params = {
+            ts: 1588591511721,
+            method: 'GET',
+            path: '/api/markets',
+        };
+
+        const key = 'T4lPid48QtjNxjLUFOcUZghD7CUJ7sTVsfuvQZF2'
+        const signature = 'dbc62ec300b2624c580611858d94f2332ac636bb86eccfa1167a7777c496ee6f'
+
+        expect(createHmacSignature(params, key)).toBe(signature);
+    });
+
+    it('should return valid signature with body', () => {
+        const params = {
+            ts: 1588591856950,
+            method: 'POST',
+            path: '/api/orders',
+            body: '{"market": "BTC-PERP", "side": "buy", "price": 8500, "size": 1, "type": "limit", "reduceOnly": false, "ioc": false, "postOnly": false, "clientId": null}'
+        };
+
+        const key = 'T4lPid48QtjNxjLUFOcUZghD7CUJ7sTVsfuvQZF2'
+        const signature = 'c4fbabaf178658a59d7bbf57678d44c369382f3da29138f04cd46d3d582ba4ba'
+
+        expect(createHmacSignature(params, key)).toBe(signature);
+    });
 });
