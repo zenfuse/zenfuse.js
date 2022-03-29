d7139c1afaacc5309ed606ecef0129457e59d470
Vyacheslav <vyacheslav@izzqz.me>
2022-03-29 20:34:01 +0300

    Add await for cache request in master test

--- tests/master.test.js
+++ tests/master.test.js
@@ -59,7 +59,7 @@ module.exports = function masterTest(Exchange, env) {
             await exchange.cache.globalCache.updatingPromise;
         });
 
-        describe('ping()', () => {
+        describe.only('ping()', () => {
             it('should be defined', () => {
                 expect(exchange.ping).toBeDefined();
             });
@@ -325,7 +325,7 @@ module.exports = function masterTest(Exchange, env) {
             });
         });
 
-        describe('fetchBalances()', () => {
+        describe.only('fetchBalances()', () => {
             it('should be defined', () => {
                 expect(exchange.fetchBalances).toBeDefined();
             });
