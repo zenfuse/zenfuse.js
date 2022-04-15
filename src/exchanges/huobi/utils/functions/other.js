28141c8b2b82c07c540053b23105d5713d7d4c87
Vyacheslav <vyacheslav@izzqz.me>
2022-04-15 07:11:33 +0000

    Extract createHmacSignature as util function

--- src/exchanges/huobi/utils/functions/other.js
+++ src/exchanges/huobi/utils/functions/other.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const { createHmac } = require('crypto');
+
+const createHmacSignature = (method, url, queryString, privateKey) => {
+    const preSignedText = `${method}\napi.huobi.pro\n/${url}\n${queryString.toString()}`;
+
+    return createHmac('sha256', privateKey)
+        .update(preSignedText)
+        .digest('base64');
+};
+
+module.exports = {
+    createHmacSignature,
+};
