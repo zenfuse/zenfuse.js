aeb04e5c3b81907f84bb6e3578d4d3906ce008e2
Vyacheslav <vyacheslav@izzqz.me>
2022-04-19 12:07:34 +0000

    Refactor transformOkxOrder function

--- src/exchanges/okx/utils/functions/transformation.js
+++ src/exchanges/okx/utils/functions/transformation.js
@@ -44,47 +44,37 @@ const transformZenfuseOrder = (zOrder) => {
 /**
  * OKX -> Zenfuse
  *
- * @param {*} fOrder Order from OKX
- * @param xOrder
- * @param zInitialOrder
+ * @param {*} xOrder Order from OKX
  * @returns {Order} Zenfuse Order
  */
-const transformOkxOrder = (xOrder, zInitialOrder = {}) => {
+const transformOkxOrder = (xOrder) => {
     /**
      * @type {Order}
      */
     const zOrder = {};
-    xOrder = xOrder.data ? xOrder.data[0] : xOrder;
 
     zOrder.id = xOrder.ordId;
-    if (Object.entries(zInitialOrder).length === 0) {
-        zOrder.timestamp = parseFloat(xOrder.cTime);
-        zOrder.symbol = xOrder.instId.replace('-', '/');
-        zOrder.type = xOrder.ordType;
-        zOrder.side = xOrder.side;
-        zOrder.quantity = parseFloat(xOrder.sz);
-        zOrder.price = xOrder.px ? parseFloat(xOrder.px) : undefined;
-        // zOrder.trades = bOrder.fills; // TODO: Fill commision counter
 
-        if (xOrder.state === 'live' || xOrder.state === 'partially_filled') {
+    zOrder.timestamp = parseFloat(xOrder.cTime);
+    zOrder.symbol = xOrder.instId.replace('-', '/');
+    zOrder.type = xOrder.ordType;
+    zOrder.side = xOrder.side;
+    zOrder.quantity = parseFloat(xOrder.sz);
+
+    if (xOrder.px) {
+        zOrder.price = parseFloat(xOrder.px);
+    }
+
+    switch (xOrder.state) {
+        case 'live':
+        case 'partially_filled':
             zOrder.status = 'open';
-        } else if (xOrder.state === 'filled') {
+            break;
+        case 'filled':
             zOrder.status = 'close';
-        } else {
+            break;
+        default:
             zOrder.status = xOrder.state;
-        }
-    } else {
-        zOrder.timestamp = zInitialOrder.timestamp
-            ? zInitialOrder.timestamp
-            : Date.now();
-        zOrder.symbol = zInitialOrder.symbol;
-        zOrder.type = zInitialOrder.type;
-        zOrder.side = zInitialOrder.side;
-        zOrder.quantity = parseFloat(zInitialOrder.quantity);
-        zOrder.price = zInitialOrder.price
-            ? parseFloat(zInitialOrder.price)
-            : undefined;
-        zOrder.status = zInitialOrder.status ? zInitialOrder.status : 'open';
     }
 
     return zOrder;
