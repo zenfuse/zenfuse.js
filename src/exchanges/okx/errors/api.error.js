439ae96c9ae20ce859cc977f7d073d49efddb186
Vyacheslav <vyacheslav@izzqz.me>
2022-04-21 13:27:29 +0000

    Fix OkxApiError constructor

--- src/exchanges/okx/errors/api.error.js
+++ src/exchanges/okx/errors/api.error.js
@@ -5,7 +5,7 @@ class OkxApiError extends ZenfuseBaseError {
      * @param {import('got').HTTPError} err
      */
     constructor(err) {
-        super(err.response.body.error);
+        super(err.response.body.msg);
         this.response = err.response.body;
         this.httpError = err;
     }
