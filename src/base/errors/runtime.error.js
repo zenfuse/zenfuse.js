d37fb0d779db073ec03fd0d2df99b5d7ae324437
Vyacheslav <vyacheslav@izzqz.me>
2022-04-26 21:02:17 +0000

    Add todo

--- src/base/errors/runtime.error.js
+++ src/base/errors/runtime.error.js
@@ -3,7 +3,9 @@ class ZenfuseRuntimeError extends Error {
      * Descriptions by error code
      */
     static details = {
-        ZEFU_CACHE_UNSYNC: 'Zenfuse global cache unsynced',
+        // TODO: Rename this error code
+        // eslint-disable-next-line @cspell/spellchecker
+        ZEFU_CACHE_UNSYNC: 'Zenfuse global cache out of sync',
         ZEFU_ORDER_NOT_FOUND: 'Required order not found',
     };
 
