dab3b51421ddf0f2bf02c08229ba0ab5085107c7
Deadvitekchpool <vitek190499@gmail.com>
2022-05-03 06:02:47 +0000

    unit tests are back

---
+++ src/exchanges/ftx/ftx.spec.js
@@ -0,0 +1,56 @@
+const { FTX } = require('zenfuse');
+
+let exchange = new FTX['spot']();
+
+describe('transfromZenfuseOrder()', () => {
+    it('should transform limit order', () => {
+        const order = {
+            symbol: 'BTC/ETH',
+            side: 'buy',
+            type: 'limit',
+            price: 69.6969,
+            quantity: 0.02323,
+            extra: 'whenbinance',
+        };
+
+        const expectation = {
+            market: 'BTC/ETH',
+            side: 'buy',
+            type: 'limit',
+            price: 69.6969,
+            size: 0.02323,
+            extra: 'whenbinance',
+        };
+
+        expect(exchange.transformZenfuseOrder(order)).toStrictEqual(
+            expectation,
+        );
+    });
+});
+
+describe('transformFtxOrder()', () => {
+    const OrderSchema = require('../../base/schemas/openOrder');
+
+    it('should transform order', () => {
+        const ftxCreatedOrder = {
+            createdAt: '2019-03-05T09:56:55.728933+00:00',
+            filledSize: 0,
+            id: 9596912,
+            market: 'ZEFU/USDT',
+            price: 10,
+            remainingSize: 31431,
+            side: 'sell',
+            size: 31431,
+            status: 'open',
+            type: 'limit',
+            reduceOnly: false,
+            ioc: false,
+            postOnly: false,
+            clientId: null,
+        };
+
+        const result = exchange.transformFtxOrder(ftxCreatedOrder);
+
+        expect(result).toMatchSchema(OrderSchema);
+    });
+});
