e2e68d2f794bae7fd95aab3025f3568bd677f78d
puzz13 <puzz13one@gmail.com>
2022-05-25 20:22:35 +0300

    Update bibox.spec.js

--- src/exchanges/bibox/bibox.spec.js
+++ src/exchanges/bibox/bibox.spec.js
@@ -10,158 +10,47 @@ describe('transformZenfuseOrder()', () => {
         };
 
         const expectation = {
-            symbol: 'BTCETH',
-            side: 'SELL',
-            type: 'MARKET',
-            quantity: '0.000001',
+            pair: 'BTC_ETH',
+            order_side: 'SELL',
+            order_type: 'MARKET',
+            amount: '0.000001',
         };
 
         expect(BiboxBase.prototype.transformZenfuseOrder(order)).toEqual(
-            expectation,
-        );
-    });
-
-    it('should add default timeInForce for limit order', () => {
-        const order = {
-            symbol: 'BTC/ETH',
-            side: 'buy',
-            type: 'limit',
-            quantity: '0.001',
-        };
-
-        const expectation = {
-            symbol: 'BTCETH',
-            side: 'BUY',
-            type: 'LIMIT',
-            quantity: '0.001',
-        };
-
-        expect(BiboxBase.prototype.transformZenfuseOrder(order)).toEqual(
-            expectation,
-        );
-    });
-
-    it('should pass custom timeInForce for limit order', () => {
-        const order = {
-            symbol: 'BTC/ETH',
-            side: 'buy',
-            type: 'limit',
-            quantity: '0.001',
-            extra: 'whenbinance',
-        };
-
-        const expectation = {
-            symbol: 'BTCETH',
-            side: 'BUY',
-            type: 'LIMIT',
-            quantity: '0.001',
-            extra: 'whenbinance',
-        };
-
-        expect(BinanceBase.prototype.transformZenfuseOrder(order)).toEqual(
             expectation,
         );
     });
 });
 
-describe('transformBinanceOrder()', () => {
+describe('transformBiboxOrder()', () => {
     const OrderSchema = require('../../base/schemas/openOrder').omit({
         symbol: true,
     });
 
     it('should transform order', () => {
-        const binanceCreatedOrder = {
-            symbol: 'BNBUSDT',
-            orderId: 5114608,
-            orderListId: -1,
-            clientOrderId: 'nVuwTgVfxQtsMV9uuMMXxL',
-            transactTime: 1637596926709,
-            price: '0.00000000',
-            origQty: '1.00000000',
-            executedQty: '1.00000000',
-            cumulativeQuoteQty: '1.00000000',
-            status: 'FILLED',
-            timeInForce: 'GTC',
-            type: 'MARKET',
-            side: 'BUY',
-            fills: [
-                {
-                    price: '576.30000000',
-                    qty: '0.77000000',
-                    commission: '0.00000000',
-                    commissionAsset: 'BNB',
-                    tradeId: 239238,
-                },
-                {
-                    price: '577.00000000',
-                    qty: '0.23000000',
-                    commission: '0.00000000',
-                    commissionAsset: 'BNB',
-                    tradeId: 239239,
-                },
-            ],
+        const biboxCreatedOrder = {
+            id: "12874181782731851",
+            createdAt: 1604309861000,
+            account_type: 0,
+            pair: "BIX_USDT",
+            coin_symbol: "BIX",
+            currency_symbol: "USDT",
+            order_side: 2,
+            order_type: 2,
+            price: "0.054330",
+            deal_price: "0.000000",
+            amount: "100.0000",
+            money: "5.43300000",
+            deal_amount: "0.0000",
+            deal_percent: "0.00%",
+            deal_money: "0.00000000",
+            deal_count: 0,
+            status: 1,
+            unexecuted: "100.0000"
         };
 
         expect(
-            BinanceBase.prototype.transformBinanceOrder(binanceCreatedOrder),
+            BiboxBase.prototype.transformBiboxOrder(biboxCreatedOrder),
         ).toMatchSchema(OrderSchema);
-    });
-});
-
-describe('assignDefaultsInOrder()', () => {
-    const DEFAULTS = {
-        limit: {
-            timeInForce: 'GTC',
-        },
-        market: {},
-    };
-
-    it('should add default timeInForce for limit order', () => {
-        const order = {
-            symbol: 'BTC/ETH',
-            side: 'buy',
-            type: 'limit',
-            quantity: '0.001',
-        };
-
-        const expectation = {
-            symbol: 'BTC/ETH',
-            side: 'buy',
-            type: 'limit',
-            quantity: '0.001',
-            timeInForce: 'GTC',
-        };
-
-        const output = BinanceBase.prototype.assignDefaultsInOrder(
-            order,
-            DEFAULTS,
-        );
-
-        expect(output).toMatchObject(expectation);
-        expect(output.timeInForce).toBe(expectation.timeInForce);
-    });
-
-    it('should add defaults for market order', () => {
-        const order = {
-            symbol: 'BTCETH',
-            side: 'BUY',
-            type: 'MARKET',
-            quantity: '0.001',
-        };
-
-        const expectation = {
-            symbol: 'BTCETH',
-            side: 'BUY',
-            type: 'MARKET',
-            quantity: '0.001',
-        };
-
-        const output = BinanceBase.prototype.assignDefaultsInOrder(
-            order,
-            DEFAULTS,
-        );
-
-        expect(output).toMatchObject(expectation);
-        expect(output.timeInForce).toBeUndefined();
     });
 });
