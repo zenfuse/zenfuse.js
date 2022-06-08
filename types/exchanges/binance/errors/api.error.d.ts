010397801e2e3f40cb6e6a95c1141fefeba8826c
Vyacheslav <vyacheslav@izzqz.me>
2022-06-08 13:05:34 +0000

    Build types

--- types/exchanges/binance/errors/api.error.d.ts
+++ types/exchanges/binance/errors/api.error.d.ts
@@ -3,12 +3,12 @@ export = BinanceApiException;
  * @see https://binance-docs.github.io/apidocs/spot/en/#error-codes
  */
 declare class BinanceApiException extends ExchangeBaseException {
-    static codesMap: Map<number, symbol>;
+    static codesMap: Map<number, string>;
     /**
      * @param {import('got').HTTPError} err
      */
     constructor(err: import('got').HTTPError);
-    code: symbol;
+    code: string;
     response: unknown;
     httpError: import("got").HTTPError;
 }
