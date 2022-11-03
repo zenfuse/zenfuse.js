05f0c0f99b647511bf92814320680ec3197051d9
Vyacheslav Melnikov <v@izzqz.me>
2022-11-03 21:19:27 +0300

    Update docusaurus.config.js

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'zenfuse.js',
-    tagline: 'A comprehensive crypto trading library for NodeJS',
+    tagline: 'Comprehensive crypto trading library for NodeJS',
     url: 'https://zenfuse.js.org/',
     baseUrl: '/',
     onBrokenLinks: 'throw',
