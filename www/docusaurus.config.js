19419295e77066daa2be0e29d966db4249beff36
izzqz <v@izzqz.me>
2022-11-10 19:37:19 +0100

    Remove ru locale

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
     // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
-        locales: ['en', 'ru'],
+        locales: ['en'],
     },
 
     presets: [
