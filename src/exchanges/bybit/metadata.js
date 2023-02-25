8aa1f9cfb580ef06a2833472734f729d301beb66
izzqz <v@izzqz.me>
2023-02-25 08:34:42 +0100

    Add metadata

---
+++ src/exchanges/bybit/metadata.js
@@ -0,0 +1,24 @@
+/**
+ * @enum {import('../..').timeInterval}
+ */
+const timeIntervals = {
+    '1m': 'm1',
+    '3m': 'm3',
+    '5m': 'm5',
+    '15m': 'm15',
+    '30m': 'm30',
+    '1h': 'h1',
+    '2h': 'h2',
+    '4h': 'h4',
+    '6h': 'h6',
+    '8h': 'h8',
+    '12h': 'h12',
+    '1d': 'd1',
+    '3d': 'd3',
+    '1w': 'w1',
+    '1M': 'M1',
+};
+
+module.exports = {
+    timeIntervals,
+};
